ccead9384a4b45e62276765938d6eb9df079386b
Kevin Yee <kevinyee1993@gmail.com>
2018-11-29 14:46:48 -0800

    Set up the project

---
+++ src/components/search_bar.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class SearchBar extends Component {
+  render() {
+    return(
+      <div>
+        Hello I am the search bar!
+      </div>
+    );
+  }
+}
