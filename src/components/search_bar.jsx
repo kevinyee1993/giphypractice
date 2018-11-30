df2b49186711f9d602387968c749a536938a24e5
Kevin Yee <kevinyee1993@gmail.com>
2018-11-29 16:26:03 -0800

    Refresh react/redux

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-export default class SearchBar extends Component {
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { simpleAction } from '../actions/simpleAction'
+
+
+class SearchBar extends Component {
   render() {
     return(
       <div>
-        Hello I am the search bar!
+          <label htmlFor="gif-search">What GIFs u want?</label>
+          <input type="text" name="gif-search"></input>
+          <button onClick={ this.props.action }>Search</button>
+        <div>Hello</div>
+        <div>{ this.props.test.result }</div>
       </div>
     );
   }
 }
+
+function mapStateToProps(state) {
+  return( {
+    test: state.simpleReducer
+  })
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({
+    action: simpleAction
+  }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SearchBar);
