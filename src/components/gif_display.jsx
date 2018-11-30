a2d43905048c16fab39dfe2cb878b1d03b2e30c7
Kevin Yee <kevinyee1993@gmail.com>
2018-11-30 00:36:01 -0800

    Extract correct url and show them on the page after searching

--- src/components/gif_display.jsx
+++ src/components/gif_display.jsx
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class GifDisplay extends Component {
+class GifDisplay extends Component {
   render() {
-    return(
-      <div>
-        Hello I am the gif display!
-      </div>
-    );
+    console.log(this.props.gifsList);
+    if(this.props.gifsList.length === 0 ) {
+      return(
+        <div>
+          Hello I am the gif display!
+        </div>
+      );
+    }
+    else {
+      return(
+        <div>
+          <ul>
+            {
+              this.props.gifsList.map( (gif, index) => {
+                return <li key={ index }>
+                  <img src={ gif.images.original.url }></img>
+                </li>
+              } )
+            }
+          </ul>
+        </div>
+      )
+    }
   }
 }
+
+function mapStateToProps(state) {
+  return({
+    gifsList: state.gifsList
+  })
+}
+
+export default connect(mapStateToProps)(GifDisplay);
--- src/reducers/gifReducer.js
+++ src/reducers/gifReducer.js
@@ -1,6 +1,6 @@
 import { UPDATE_GIFS } from '../actions/update_gif_list';
 
-export default function (state = {}, action) {
+export default function (state = [], action) {
   switch(action.type) {
     case UPDATE_GIFS:
       return action.payload;
